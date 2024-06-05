7ce0d46b93ad604231d5550698e7e488593e3f72
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-05 12:56:52 +0200

    fixed typo

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -68,6 +68,6 @@ contract Variables5 {
 contract Variables6 {
     string name1 = "Name1";
     string private name2 = "Name2";
-    string internal name2 = "Name3";
+    string internal name3 = "Name3";
     string public name4 = "Name4";
 }
