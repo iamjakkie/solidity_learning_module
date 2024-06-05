06d042d4535685342eef2afa2e3f17ab365adb2e
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-05 15:50:18 +0200

    functions

--- contracts/Functions.sol
+++ contracts/Functions.sol
@@ -42,3 +42,35 @@ contract Functions3 {
         count = addNumbers(count, 1);
     }
 }
+
+contract Functions4 {
+    uint public count;
+
+    function increment1() public {
+        count = count + 1;
+    }
+
+    function increment2() public {
+        increment1();
+    }
+
+    function increment3() private {
+        count = count + 1;
+    }
+
+    function increment4() public {
+        increment3();
+    }
+
+    function increment5() external {
+        count = count + 1;
+    }
+
+    function increment6() internal {
+        count = count + 1;
+    }
+
+    function increment7() public {
+        increment6();
+    }
+}
--- test/Functions.js
+++ test/Functions.js
@@ -32,4 +32,74 @@ describe('Functions', () => {
             expect(await contract.getName()).to.equal('New Name')
         })
     })
+
+    describe('Example 2', () => {
+        let contract
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Functions2');
+            contract = await Contract.deploy();
+        })
+
+        it('Calls a function inside another function', async () => {
+            await contract.increment();
+            expect(await contract.count()).to.equal(1)
+        })
+    })
+
+    describe('Example 3', () => {
+        let contract
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Functions3');
+            contract = await Contract.deploy();
+        })
+
+        it('Calls a function outside of the contract', async () => {
+            await contract.increment();
+            expect(await contract.count()).to.equal(1)
+        })
+    })
+
+    describe('Example 4', () => {
+        let contract
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Functions4');
+            contract = await Contract.deploy();
+        })
+
+        it('Calls a public function outside of the contract', async () => {
+            await contract.increment1();
+            expect(await contract.count()).to.equal(1)
+        })
+
+        it('Calls a public function from another function', async () => {
+            await contract.increment2();
+            expect(await contract.count()).to.equal(1)
+        })
+
+        it('Cannot call a private function outside of the contract', async () => {
+            await expect(contract.increment3).to.be.undefined
+        })
+
+        it('Calls a private function from another function', async () => {
+            await contract.increment4();
+            expect(await contract.count()).to.equal(1)
+        })
+
+        it('Calls an external function outside of the contract', async () => {
+            await contract.increment5();
+            expect(await contract.count()).to.equal(1)
+        })
+
+        it('Cannot call an internal function outside of the contract', async () => {
+            await expect(contract.increment6).to.be.undefined
+        })
+
+        it('Calls an internal function from another function', async () => {
+            await contract.increment7();
+            expect(await contract.count()).to.equal(1)
+        })
+    })
 })
