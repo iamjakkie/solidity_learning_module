7fcf791f2a22a216f0e3cca5fbf2a06faa77daad
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-09 21:49:19 +0200

    conditionals

--- contracts/Conditionals.sol
+++ contracts/Conditionals.sol
@@ -12,4 +12,27 @@ contract Conditionals1 {
             return "odd";
         }
     }
+
+    function evenOrOdd2(uint x) public pure returns (string memory) {
+        if (x % 2 == 0) {
+            return "even";
+        }
+        return "odd";
+    }
+
+    function evenOrOdd3(uint x) public pure returns (string memory) {
+        return x % 2 == 0 ? "even" : "odd";
+    }
+}
+
+contract Conditionals2 {
+    function checkNumber(uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 100) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 }
--- test/Conditionals.js
+++ test/Conditionals.js
@@ -7,6 +7,41 @@ const ether = tokens;
 
 describe('Conditionals', () => {
     describe('Example 1', () => {
-        it()
+        let contract;
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Conditionals1');
+            contract = await Contract.deploy();
+        })
+
+        it('Contract with basic if else statement', async () => {
+            expect(await contract.evenOrOdd1(2)).to.equal('even')
+            expect(await contract.evenOrOdd1(3)).to.equal('odd')
+        })
+
+        it('Contract with just if statement', async () => {
+            expect(await contract.evenOrOdd2(2)).to.equal('even')
+            expect(await contract.evenOrOdd2(3)).to.equal('odd')
+        })
+
+        it('Contract with return ternary operator', async () => {
+            expect(await contract.evenOrOdd3(2)).to.equal('even')
+            expect(await contract.evenOrOdd3(3)).to.equal('odd')
+        })
+    })
+
+    describe('Example 2', () => {
+        let contract;
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Conditionals2');
+            contract = await Contract.deploy();
+        })
+
+        it('Contract with complex if else statement', async () => {
+            expect(await contract.checkNumber(9)).to.equal(0)
+            expect(await contract.checkNumber(10)).to.equal(1)
+            expect(await contract.checkNumber(100)).to.equal(2)
+        })
     })
 })
