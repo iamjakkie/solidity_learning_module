d0c1ae0ac989712420ade0c766ff55a48112e0e0
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-10 14:15:34 +0200

    fixed get arr

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -7,4 +7,36 @@ contract Arrays1 {
     uint[] public array1 = [1,2,3];
     uint[] public array2;
     uint[10] public array3;
+
+    function getArray1() public view returns (uint[] memory) {
+        return array1;
+    }
+
+    function getArray2() public view returns (uint[] memory) {
+        return array2;
+    }
+
+    function getArray3() public view returns (uint[10] memory) {
+        return array3;
+    }
+}
+
+contract Arrays2 {
+    uint[] public array;
+
+    function get(uint i) public view returns (uint) {
+        return array[i];
+    }
+
+    function getArray() public view returns (uint[] memory) {
+        return array;
+    }
+
+    function addToArray(uint i) public {
+        array.push(i);
+    }
+
+    function length() public view returns (uint) {
+        return array.length;
+    }
 }
--- test/Arrays.js
+++ test/Arrays.js
@@ -21,12 +21,37 @@ describe('Array', () => {
         })
 
         it('Contract with declared and not initialized array', async () => {
-            expect(await contract.array2(0)).to.throw(Error);
+            expect(await contract.getArray2()).to.deep.equal([]);
         })
 
-        // it('Contract with declared and initialized array with fixed size', async () => {
-        //     expect(contract.array3).to.deep.equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        // })
+        it('Contract with declared and initialized array with fixed size', async () => {
+            expect(await contract.getArray3()).to.deep.equal([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        })
 
+    })
+
+    describe('Example 2', () => {
+        let contract;
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Arrays2');
+            contract = await Contract.deploy();
+        })
+
+        it('Add to array', async () => {
+            await contract.addToArray(1)
+            await contract.addToArray(2)
+            await contract.addToArray(3)
+            expect(await contract.get(0)).to.equal(1)
+            expect(await contract.get(1)).to.equal(2)
+            expect(await contract.get(2)).to.equal(3)
+        })
+
+        it('Get array', async () => {
+            await contract.addToArray(1)
+            await contract.addToArray(2)
+            await contract.addToArray(3)
+            expect(await contract.getArray()).to.deep.equal([1, 2, 3])
+        })
     })
 })
