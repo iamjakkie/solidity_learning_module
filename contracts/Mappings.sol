723b9beb65e9165b56222e1c99c5fc3fca63e335
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-11 20:42:52 +0200

    Mappings

--- contracts/Mappings.sol
+++ contracts/Mappings.sol
@@ -5,15 +5,41 @@ import "hardhat/console.sol";
 
 contract Mappings1 {
     mapping(uint => string) public names;
-    mapping(uint => address) public addresses;
-    mapping(address => uint) public balances;
 
     constructor() {
         names[1] = "Adam";
         names[2] = "Ben";
     }
 
-    function myFunction() {
+}
 
+contract Mappings2 {
+
+    struct Book {
+        string author;
+        string title;
+    }
+
+    mapping(uint => Book) public books;
+
+    constructor() {
+        books[1] = Book("Leo Tolstoy", "War and Peace");
+        books[2] = Book("F. Scott Fitzgerald", "The Great Gatsby");
+    }
+}
+
+contract Mappings3 {
+    mapping(uint => string) public myMapping;
+
+    function get(uint _id) public view returns (string memory) {
+        return myMapping[_id];
+    }
+
+    function set(uint _id, string memory _value) public {
+        myMapping[_id] = _value;
+    }
+
+    function remove(uint _id) public {
+        delete myMapping[_id];
     }
 }
--- test/Mappings.js
+++ test/Mappings.js
@@ -13,5 +13,50 @@ describe('Mappings', () => {
             const Contract = await ethers.getContractFactory('Mappings1');
             contract = await Contract.deploy();
         })
+
+        it('Assigned names', async () => {
+            expect(await contract.names(1)).to.equal('Adam')
+            expect(await contract.names(2)).to.equal('Ben')
+        })
+    })
+
+    describe('Example 2', () => {
+        let contract;
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Mappings2');
+            contract = await Contract.deploy();
+        })
+
+        it('Assigned custom structs', async () => {
+            let result = await contract.books(1)
+            expect(result[0]).to.equal('Leo Tolstoy')
+            expect(result[1]).to.equal('War and Peace')
+
+            result = await contract.books(2)
+            expect(result[0]).to.equal('F. Scott Fitzgerald')
+            expect(result[1]).to.equal('The Great Gatsby')
+        })
+    })
+
+    describe('Example 3', () => {
+        let contract;
+
+        beforeEach(async () => {
+            const Contract = await ethers.getContractFactory('Mappings3');
+            contract = await Contract.deploy();
+            await contract.set(1, 'one')
+            await contract.set(2, 'two')
+        })
+
+        it('Getting and setting values', async () => {
+            expect(await contract.get(1)).to.equal('one')
+            expect(await contract.get(2)).to.equal('two')
+        })
+
+        it('Remove value', async () => {
+            await contract.remove(1)
+            expect(await contract.get(1)).to.equal('')
+        })
     })
 })
