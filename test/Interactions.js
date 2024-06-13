a0e93509e0aad796538fa259bff2604963cb93fd
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-13 15:01:44 +0200

    interactions

---
+++ test/Interactions.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (amount) => ethers.parseEther(amount.toString());
+
+const ether = tokens;
+
+describe('Inheritance', () => {
